86a14375237a8ec8c0742d272557fba6d4f8735b
Kevin Nyangena <nyangena.kevin@students.jkuat.ac.ke>
2022-05-25 20:23:00 +0100

    Added Black Lotus print statement

--- BlackLotus/ViewController.swift
+++ BlackLotus/ViewController.swift
@@ -12,7 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        print("The rare is Black Lotus")
+    
     }
+    
+    
 
 
 }
